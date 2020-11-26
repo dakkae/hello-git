22956fa89bafd7ccd2a037c37cc566b1562f05bf
daka <dky0830@gmail.com>
2020-11-26 08:04:50 +0000

    Fourth commit

--- hello.java
+++ hello.java
@@ -2,5 +2,6 @@ public class hello {
 	public static void main(String[] args) {
 		System.out.println("Hello, world from GIT!");
 		System.out.println("Change after First commit!");
+        System.out.println("Change after Pushing to remote!");
 	}
 }
